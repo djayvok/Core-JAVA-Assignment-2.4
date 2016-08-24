a586ee6751a45c00944153ed1761138a9ed46a1b
Dhananjaya <djay.vok@gmail.com>
2016-08-24 13:37:37 +0100

    Update and rename .gitignore to Largest.java

---
+++ Largest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Largest{
+public static void main(String []args){
+int a, b, c, d;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter all three numbers:");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+        d = (a>b)?(a>c?a:c):(b>c?b:c);
+        System.out.println("Largest Number:"+d);
+}
+}
